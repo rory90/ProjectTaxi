755422ae37e3e1a145260ba71a6d9abcf3833a5a
rory90 <rory.adamson89@gmail.com>
2019-03-26 12:08:15 -0500

    Added Booking and Cab class

---
+++ Taxi_Project/src/main/java/com/utech/ap/Taxi_Project/Entity/Booking.java
@@ -0,0 +1,61 @@
+package com.utech.ap.Taxi_Project.Entity;
+
+public class Booking {
+
+	private int distance;
+	private static int bookingId;
+	private int cabId;
+	private int customerId;
+	private int billingAmount;
+	
+	
+	public static int getBookingId() {
+		return bookingId;
+	}
+
+
+	public void setBookingId(int bookingId) {
+		Booking.bookingId = bookingId;
+	}
+
+
+	public int getDistance() {
+		return distance;
+	}
+
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+
+	public int getCabId() {
+		return cabId;
+	}
+
+
+	public void setCabId(int cabId) {
+		this.cabId = cabId;
+	}
+
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+
+	public int getBillingAmount() {
+		return billingAmount;
+	}
+
+
+	public void setBillingAmount(int billingAmount) {
+		this.billingAmount = billingAmount;
+	}
+
+}
